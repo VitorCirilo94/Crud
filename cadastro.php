52e40c69445b8a5a71bbf67db7018facaa01728f
VitorCirilo94 <vitorcirilo@hotmail.com>
2024-03-18 17:50:54 -0300

    versão 2

--- cadastro.php
+++ cadastro.php
@@ -7,3 +7,32 @@ $query_select = "SELECT login FROM usuarios WHERE login = "$email"";
 $select = mysql_query($query_select,$connect);
 $array = mysql_fetch_array($select);
 $logarray = $array["E-mail"];
+
+if($email == ""|| $email == null){
+    echo"<script language="javascript" type"text/javascript">
+    alert("O Campo E-mail deve ser preenchido")window.location.href="cadastro.html";<\scriot>";
+}
+else{
+    if($logarray == $email){
+
+        echo"<script language="javascript" type="text/javascript">
+        alert("Esse email já existe");window.location.href="cadastro.html";</script>;
+        die();
+    }
+else{
+    $query = "INSERT INTO usuarios (email, senha) VALUES ("$email","$senha")";
+    $insert = mysql_query($query,$connect);
+
+    if($insert){
+        echo"<script language="javascript" type="texte/javascript">
+        alert("Usuário cadastrado com sucesso");window.location.href="email.html"</script>;
+    }
+    else{
+        echo"<script languagem="javascript" typ="text/javascript">
+        alert("Não foi possivel cadastrar esse usuário")window.location.href="cadastro.html"</script>"
+    }
+}
+
+?>
+
+
---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+$email = $_POST["E-mail"];
+$acessar = $_POST["Acessar"];
+$senha = md5($_POST["Senha"])
+$connect = mysql_connect("nome_do_servidor","nome_de_usuario","senha");
+$db = mysql_select_db("cadastro.sql");
+
+  if(isset($acessar)) {
+    $verifica = mysql_query("SELECT * FROM usuario WHERE email = "$email" AND senha = "$senha"") or die("erro ao selecionar");
+    if(mysql_num_rows($verifica)<=0){
+        echo"script language="javascript" type="text/javascript">
+        alert("Login e/ou senha incorretos")window.location.href="email.html";>/script>";
+        die();
+    }
+    else{
+        setcookie("email",$email);
+        header("location.index.php");
+    }
+  }
+  ?>
+  
